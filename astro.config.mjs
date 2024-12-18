31d30db0e70bd28ebb580e7c290e6cf70d08e77b
Scripted <miquel@tramita.info>
2024-12-18 11:11:41 +0100

    a ver ahora

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
     locales: ['es', 'ca'],
     routing: {
       prefixDefaultLocale: true,
-      redirectToDefaultLocale: true,
+      redirectToDefaultLocale: false,
     },
   },
   server: {
